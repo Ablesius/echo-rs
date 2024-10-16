28f3184114af67222179f06eeaa5b68711da3dd9
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-10-16 13:02:05 +0200

    refactor: Move code to main
    The setup of the code base distributed over run, Config, main and lib.rs
    was confusing for such a simple program. So I refactored everything back
    into a single main(). echo() as function remains but it's much simpler
    to read and call now.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,69 @@
-use echo_rs::run;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let echo_str = args[1..].join(" ");
-    run(&echo_str);
+    let echoes = echo(&args[1..]);
+    println!("{}", echoes);
 }
+
+/// Echo the input.
+pub fn echo(strings: &[String]) -> String {
+    String::from(strings.join(" ").as_str())
+}
+
+#[test]
+fn one_word() {
+    assert_eq!(echo(&[String::from("foo")]), "foo");
+}
+
+#[test]
+fn two_words() {
+    assert_eq!(echo(&[String::from("foo bar")]), "foo bar");
+}
+
+#[test]
+fn multiple_words() {
+    assert_eq!(
+        echo(&[String::from("foo bar baz fkjdlshf098432whvds ofYq")]),
+        "foo bar baz fkjdlshf098432whvds ofYq"
+    );
+}
+
+#[test]
+fn multiple_words_joined() {
+    let strs = [
+        String::from("foo"),
+        String::from("bar"),
+        String::from("baz"),
+        String::from("fkjdlshf098432whvds"),
+        String::from("ofYq"),
+    ];
+    assert_eq!(strs.join(" "), "foo bar baz fkjdlshf098432whvds ofYq");
+}
+
+#[test]
+fn one_char() {
+    assert_eq!(echo(&[String::from("f")]), "f");
+}
+
+#[test]
+fn numbers() {
+    assert_eq!(echo(&[String::from("1")]), "1");
+    assert_eq!(echo(&[String::from("2.5")]), "2.5");
+    assert_eq!(echo(&[String::from("0x123")]), "0x123");
+}
+
+#[test]
+fn non_latin() {
+    assert_eq!(echo(&[String::from("đ")]), "đ");
+    assert_eq!(echo(&[String::from("å")]), "å");
+    assert_eq!(echo(&[String::from("scheiße")]), "scheiße");
+}
+
+// #[test]
+// fn with_no_spaces() {
+//     let input = vec!["foo", "bar"];
+//     let expected = "foobar";
+//
+//     assert_eq!(echo(&input, None), expected);
+// }
