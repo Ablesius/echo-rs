5b4106d06334d9302999c84ca2f9d1ac8187c33c
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-10-17 12:00:48 +0200

    feat: add no-separate option
    It is horribly named, actually. But it works just like echo -s now.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,70 @@
-use std::env;
+use clap::Parser;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let echoes = echo(&args[1..]);
+    let cli = Cli::parse();
+
+    let echoes = {
+        // this is a bit ugly to have a double negative, but we're just cloning echo's -s
+        if !cli.separate {
+            echo(cli.strings)
+        } else {
+            unseparated_echo(cli.strings)
+        }
+    };
     println!("{}", echoes);
 }
 
+#[derive(Parser)]
+struct Cli {
+    /// Do not separate output with spaces. Defaults to false.
+    #[arg(short, action)]
+    separate: bool,
+
+    /// The string to echo
+    #[arg(trailing_var_arg = true)]
+    strings: Vec<String>,
+}
+
 /// Echo the input.
-fn echo(strings: &[String]) -> String {
-    String::from(strings.join(" ").as_str())
+fn echo(strings: Vec<String>) -> String {
+    strings.join(" ")
+}
+
+/// Echo the input but do not separate with a space.
+fn unseparated_echo(strings: Vec<String>) -> String {
+    // todo!("not implemented yet, try using without -s!")
+    strings.join("")
 }
 
 #[test]
 fn one_word() {
-    assert_eq!(echo(&[String::from("foo")]), "foo");
+    assert_eq!(echo(vec!(String::from("foo"))), "foo");
 }
 
 #[test]
 fn two_words() {
-    assert_eq!(echo(&[String::from("foo bar")]), "foo bar");
+    assert_eq!(
+        echo(vec!(String::from("foo"), String::from("bar"),)),
+        "foo bar"
+    );
 }
 
 #[test]
 fn multiple_words() {
     assert_eq!(
-        echo(&[String::from("foo bar baz fkjdlshf098432whvds ofYq")]),
+        echo(vec!(
+            String::from("foo"),
+            String::from("bar"),
+            String::from("baz"),
+            String::from("fkjdlshf098432whvds"),
+            String::from("ofYq")
+        )),
         "foo bar baz fkjdlshf098432whvds ofYq"
     );
 }
 
 #[test]
-fn multiple_words_joined() {
+fn multiple_words_joined_with_separator() {
     let strs = [
         String::from("foo"),
         String::from("bar"),
@@ -43,27 +77,26 @@ fn multiple_words_joined() {
 
 #[test]
 fn one_char() {
-    assert_eq!(echo(&[String::from("f")]), "f");
+    assert_eq!(echo(vec!(String::from("f"))), "f");
 }
 
 #[test]
 fn numbers() {
-    assert_eq!(echo(&[String::from("1")]), "1");
-    assert_eq!(echo(&[String::from("2.5")]), "2.5");
-    assert_eq!(echo(&[String::from("0x123")]), "0x123");
+    assert_eq!(echo(vec!(String::from("1"))), "1");
+    assert_eq!(echo(vec!(String::from("2.5"))), "2.5");
+    assert_eq!(echo(vec!(String::from("0x123"))), "0x123");
 }
 
 #[test]
 fn non_latin() {
-    assert_eq!(echo(&[String::from("đ")]), "đ");
-    assert_eq!(echo(&[String::from("å")]), "å");
-    assert_eq!(echo(&[String::from("scheiße")]), "scheiße");
+    assert_eq!(echo(vec!(String::from("đ"))), "đ");
+    assert_eq!(echo(vec!(String::from("å"))), "å");
+    assert_eq!(echo(vec!(String::from("scheiße"))), "scheiße");
 }
 
-// #[test]
-// fn with_no_spaces() {
-//     let input = vec!["foo", "bar"];
-//     let expected = "foobar";
-//
-//     assert_eq!(echo(&input, None), expected);
-// }
+#[test]
+fn with_no_spaces() {
+    let input = vec![String::from("foo"), String::from("bar")];
+    let expected = "foobar";
+    assert_eq!(unseparated_echo(input), expected)
+}
