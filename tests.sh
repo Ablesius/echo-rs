79d9821001a0ccc1b37457db8e02837bcd508869
Lexi Blesius <Ablesius@users.noreply.github.com>
2024-10-18 10:34:57 +0200

    ci[e2e tests]: Added -s test

--- tests.sh
+++ tests.sh
@@ -6,9 +6,13 @@ CARGO_OPTS="-q"
 echo "DEBUG MODE"
 echo "this should print 'Hello world!':"
 cargo run $CARGO_OPTS -- Hello world!
+echo "this should print 'Helloworld!':"
+cargo run $CARGO_OPTS -- -s Hello world!
 
 echo ""
 
 echo "RELEASE MODE"
 echo "this should print 'Hello world!':"
 cargo run $CARGO_OPTS --release -- Hello world!
+echo "this should print 'Helloworld!':"
+cargo run $CARGO_OPTS --release -- -s Hello world!
